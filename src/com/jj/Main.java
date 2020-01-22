d42d633a30ff5bb993878065901c319be09dbde2
JJ <jjaimes20@gmail.com>
2020-01-22 16:45:36 +0100

    proyecto creado v1

--- src/com/jj/Main.java
+++ src/com/jj/Main.java
@@ -4,5 +4,11 @@ public class Main
 {
     public static void main(String[] args) {
         System.out.println("hola que ace");
+
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+
+        }
     }
 }
